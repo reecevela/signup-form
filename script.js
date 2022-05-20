2e2e3c173646dc12cba94739d68f4b8c695ee5e9
Reece Vela <reecevela@outlook.com>
2022-05-19 20:06:17 -0500

    Add validation

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("This works");
+function checkPassword() {
+  let messageBox = document.querySelector('#message');
+  let password = document.querySelector('#password').value;
+  let confirm = document.querySelector('#confirm').value;
+
+
+  messageBox.style.color = "black";
+
+  if (password.length != 0) {
+    if (password == confirm) {
+      messageBox.textContent = "Looks good!";
+      messageBox.style.backgroundColor = "lightgreen";
+    } else {
+      messageBox.textContent = "Passwords do not match";
+      messageBox.style.backgroundColor = "lightpink";
+    }
+  } else {
+    alert("Password can't be empty!");
+    messageBox.textContent = "";
+  }
+}
+
